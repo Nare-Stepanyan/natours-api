74bd833013331658991220c54f2aeea71c7eb673
Nare <45096006+Nare-Stepanyan@users.noreply.github.com>
2022-01-18 16:49:04 +0400

    handle mongoose validation errors, invalid db ids annd duplicate db fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,22 @@
+import AppError from '../utils/appError.js';
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data: ${errors.join(', ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     err: err,
@@ -23,13 +42,18 @@ const sendErrorProd = (err, res) => {
     });
   }
 };
+
 const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+    sendErrorProd(error, res);
   }
 };
 
